0c91ed45f5b4cc7818459f9bc1cbdf0135011f0a
jaime <jaime.molina@mattersupply.co>
2020-05-03 20:47:47 -0500

    Test handler test again!

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -18,6 +18,7 @@ defmodule Servy.Handler do
     |> log
     |> route
     |> track
+    |> put_content_length
     |> format_response
   end
 
@@ -104,11 +105,18 @@ defmodule Servy.Handler do
     %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
+  def put_content_length(conv) do
+    headers = Map.put(conv.resp_headers, "Content-Length", String.length(conv.resp_body))
+    # Map.put(conv, 'resp_headers', headers)
+    %{conv | resp_headers: headers}
+  end
+
   def format_response(%Conv{} = conv) do
+    # Content-Length: #{String.length(conv.resp_body)}\r
     """
     HTTP/1.1 #{Conv.full_status(conv)}\r
-    Content-Type: #{conv.resp_content_type}\r
-    Content-Length: #{String.length(conv.resp_body)}\r
+    Content-Type: #{conv.resp_headers["Content-Type"]}\r
+    Content-Length: #{conv.resp_headers["Content-Length"]}\r
     \r
     #{conv.resp_body}
     """
